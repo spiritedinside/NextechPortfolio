9adeeb2453f9d4fe9745a600fc908ca253e1e1f8
spiritedinside <walthersvirginia@gmail.com>
2026-06-22 15:52:41 -0400

    updated about me

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ let abtMeBtn = document.querySelector(".button1");
 let projBtn = document.querySelector(".button2");
 
 abtMeBtn.addEventListener('click', function() {
-    
+    abtMeBtn.style.
 })
