ea4346f29ac778add732933986de30da5fe7f631
spiritedinside <walthersvirginia@gmail.com>
2026-07-01 09:48:09 -0400

    contact page formed

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let button = document.querySelector(".buttons");
 let abtMeBtn = document.querySelector(".button1");
 let projBtn = document.querySelector(".button2");
+let ltsAnimation = document.querySelector("#lts");
+let sameMalletAnimation = document.querySelector("#sameMallet");
+let resurrectionAnimation = document.querySelector("#resurrection");
+let fullOrchestraAnimation = document.querySelector("#fullOrchestra");
 
-abtMeBtn.addEventListener('click', function() {
-    abtMeBtn.style.
-})
+fullOrchestraAnimation.addEventListener("click", () => {
+    fullOrchestraAnimation.style. = "divImg";
+});
